c90ae28efa1e07b5cbb6aa19349c90eb1825a3a6
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2022-09-03 19:45:59 +0700

    fixed Login

--- application/views/admin/v_login.php
+++ application/views/admin/v_login.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>M-Techno | Log in</title>
+  <title>Malikussaleh Airport | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="shorcut icon" type="text/css" href="<?php echo base_url().'assets/images/favicon.png'?>">
